68bded5d5520e29f57cea4046a63c9a46e1222a7
toona <amanou.toona@gmail.com>
2021-01-11 18:46:42 +0900

    chapter2/2-3

--- chapter2/2-3-2.py
+++ chapter2/2-3-2.py
@@ -1,21 +1,42 @@
 """
 ガウス過程と機械学習
 chapter2 2.1 p39~
-ガウス分布  サンプルの作図
+ガウス分布  確率分布の作図
 """
 import numpy as np
+import matplotlib as mpl
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
 
 # ==========================
-mean = np.array([0, 0])
+mu = np.array([0, 0])
 cov = np.array([[1, 0], [0, 1]])
-sample = 1000
+
+x1 = x2 = np.arange(-5, 6, 0.1)
+x1, x2 = np.meshgrid(x1, x2)
 # ==========================
 
-data = np.random.multivariate_normal(mean, cov, sample)
+
+def gaussian(x):
+    D = cov.ndim
+    det = np.linalg.det(cov)
+
+    return (1 / np.sqrt(2 * np.pi) ** D * np.sqrt(det)) * np.exp(-0.5 * np.diag((x - mu) @ np.linalg.inv(cov) @ (x - mu).T))
+
+
+X = np.stack([np.ravel(x1), np.ravel(x2)], axis=0).T
+N = gaussian(X)
+N = N.reshape(x1.shape)
 
 # 作図
-fig, ax = plt.subplots()
-ax.scatter(data[:, 0], data[:, 1])
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.set_xlabel('x1', size=14)
+ax.set_ylabel('x2', size=14)
+ax.set_zlabel('y', size=14)
+
+ax.plot_surface(x1, x2, N, cmap=mpl.cm.coolwarm)
 plt.show()
+
 
